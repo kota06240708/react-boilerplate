ce7cbf73db752adb9fe4eb0436387ddb5cea17c9
kota06240708 <daiya50.ezweb.ne.jp@gmail.com>
2019-05-07 02:26:23 +0900

    add: slickを追加

--- src/script/component/animation/swiper.js
+++ src/script/component/animation/swiper.js
@@ -10,17 +10,86 @@ const Slides = styled.div`
   color: #fff;
 `;
 
+let SliderDom = null;
+
 const params = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToShow: 1, // 1フレーム時に表示する数
+  slidesToScroll: true, // スワイプの真偽
+
+  /**
+   * スライド時に発火
+   * @param {number} - 現在何番目のスライドか返す
+   */
+  afterChange: e => {
+    console.log(e);
+    console.log(SliderDom);
+  },
+
+  /**
+   * スライド時に発火
+   * @param {number} - 前回何番目のスライドか返す
+   */
+  beforeChange: e => {
+    console.log(e);
+  },
+
+  // ページネーションを変更できる。
+  customPaging: e => {
+    return <button> {e + 1} </button>;
+  },
+
+  /**
+   * スワイプ時に発火
+   * @param {string} - スワイプした方向を返す。
+   */
+  onSwipe: e => {
+    console.log(e);
+  }
 };
 
 const SimpleSwiper = () => (
   <div>
-    <Slider {...params}>
+    <button
+      onClick={e => {
+        e.preventDefault();
+        SliderDom.slickNext();
+      }}
+    >
+      次に進む
+    </button>
+    <button
+      onClick={e => {
+        e.preventDefault();
+        SliderDom.slickPrev();
+      }}
+    >
+      前に進む
+    </button>
+    <button
+      onClick={e => {
+        e.preventDefault();
+        SliderDom.slickPause();
+      }}
+    >
+      自動を再生停止
+    </button>
+    <button
+      onClick={e => {
+        e.preventDefault();
+        SliderDom.slickPlay();
+      }}
+    >
+      自動を再生開始
+    </button>
+    <Slider
+      ref={el => {
+        SliderDom = el;
+      }}
+      {...params}
+    >
       <Slides>Slide 1</Slides>
       <Slides>Slide 2</Slides>
       <Slides>Slide 3</Slides>
