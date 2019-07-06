b33869282893c97a624b3b07d7750fd0e8173500
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-07-06 14:06:21 +0900

    update: npmを最新にアップデート

--- system/gulp/build.js
+++ system/gulp/build.js
@@ -2,13 +2,10 @@ import gulp from 'gulp';
 import conf from '../config';
 
 gulp.task('build',
-	gulp.series(
-		'pug:prod',
-		() => {
-			return gulp
-				.src(`./${conf.src}/**/*.+(jpg|jpeg|png|gif|svg)`)
-				.pipe(gulp.dest(process.env.NODE_ENV));
-		},
-		'js:prod'
-	)
-);
+  gulp.series(
+    'pug:prod',
+    () => gulp
+      .src(`./${conf.src}/**/*.+(jpg|jpeg|png|gif|svg)`)
+      .pipe(gulp.dest(process.env.NODE_ENV)),
+    'js:prod'
+  ));
